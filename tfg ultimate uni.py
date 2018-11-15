cd33244b2a69782b31e6eaf90820234cfe2baec8
RafaCM89 <44846256+RafaCM89@users.noreply.github.com>
2018-11-15 10:51:22 +0100

    Add files via upload

--- tfg ultimate uni.py
+++ tfg ultimate uni.py
@@ -8,6 +8,14 @@ from __future__ import division
 import math
 import numpy as np
 from numpy import linalg as LA
+def quaternion_multiplication (x1, ux, y1, uy):
+    m1 = x1 * y1 - np.dot(ux, uy)
+    m2 = x1 * uy + y1 * ux + ux * uy
+    return [m1,m2]
+def conjugate_quaternion (x1, u):
+    x1 = x1
+    u = (-1)*u
+    return [x1, u]
 f = open('disco1.csv', 'r')
 i = 0;
 for line in f.readlines():
@@ -35,14 +43,6 @@ q2 = math.sin(aux_q1) * aux_q2
 qw = [q1,q2] #Ojo qw es del tipo list
 #hallamos ahora el quaternion de rotación entre dos epochs para lo cual hemos
 #de implementar antes la multiplicación de quaterniones
-def quaternion_multiplication (x1, ux, y1, uy):
-    m1 = x1 * y1 - np.dot(ux, uy)
-    m2 = x1 * uy + y1 * ux + ux * uy
-    return [m1,m2]
-def conjugate_quaternion (x1, u):
-    x1 = x1
-    u = (-1)*u
-    return [x1, u]
 aux_quaternion1 = np.array([L1[10],L1[11],L1[12],L1[13]])
 aux_quaternion2 = aux_quaternion1.astype(np.float)
 quaternion = [aux_quaternion2[0], np.array([aux_quaternion2[1],aux_quaternion2[2],aux_quaternion2[3]])]
